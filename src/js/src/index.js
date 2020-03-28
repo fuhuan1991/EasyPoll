4760e15a94a8c8b616be46836f7f9ac94be3cf33
fuhuan1991 <huanfu2019@gmail.com>
2020-03-27 21:19:49 -0400

    front end

---
+++ src/js/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Main from './Main';
+import 'antd/dist/antd.css';
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
+);
