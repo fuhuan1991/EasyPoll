02fd4adb58607aa7743e014f90d94d7b637e69c9
fuhuan1991 <huanfu2019@gmail.com>
2020-05-10 15:03:25 -0400

    add controller

--- src/main/java/com/polling/controller/PageController.java
+++ src/main/java/com/polling/controller/PageController.java
@@ -1,28 +1,88 @@
-package com.polling;
+package com.polling.controller;
 
+import com.polling.model.Poll;
+import com.polling.service.OptionService;
+import com.polling.service.PollService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-//@RestController
-//@RequestMapping("api/users")
 public class PageController {
 
-  // initial configuration page
-  @GetMapping("/admin")
-  public String admin() {
+  private final PollService pollService;
+  private final OptionService optionService;
+
+  @Autowired
+  public PageController (PollService pollService, OptionService optionService) {
+    this.pollService = pollService;
+    this.optionService = optionService;
+  }
+
+
+  /**
+   * Request of administration page for a certain poll.
+   * If this poll does not exist in DB, a error page will be returned.
+   * The content of this administration page includes some basic information,
+   * and links to share.
+   */
+  @GetMapping("/admin/{poll_id}")
+  public String admin(Model model, @PathVariable("poll_id") String poll_id) {
+
+    Poll poll = this.pollService.findPollById(poll_id);
+
+    if (poll == null) {
+      model.addAttribute("message", "The poll does not exist.");
+      return "error";
+    }
+
+    model.addAttribute("poll_id", poll_id);
+    model.addAttribute("poll_title", poll.getTitle());
+    model.addAttribute("start_time", poll.getStartTime());
+    model.addAttribute("end_time", poll.getEndTime());
+
     return "admin";
   }
 
+  /**
+   * Request of result page for a certain poll.
+   * If this poll does not exist in DB, a error page will be returned.
+   * The result of this poll shall be displayed in here.
+   */
+  @GetMapping("/result/{poll_id}")
+  public String result(Model model, @PathVariable("poll_id") String poll_id) {
+
+    Poll poll = this.pollService.findPollById(poll_id);
+
+    if (poll == null) {
+      model.addAttribute("message", "The poll does not exist.");
+      return "error";
+    }
+
+    model.addAttribute("poll", poll);
+
+    return "result";
+  }
+
+  /**
+   * Request of interaction page for a certain poll.
+   * If this poll does not exist in DB, a error page will be returned.
+   * In this page, a user can take the poll.
+   */
+  @GetMapping("/poll/{poll_id}")
+  public String poll(Model model, @PathVariable("poll_id") String poll_id) {
+
+    Poll poll = this.pollService.findPollById(poll_id);
+
+    if (poll == null) {
+      model.addAttribute("message", "The poll does not exist.");
+      return "error";
+    }
+
+    model.addAttribute("poll", poll);
+
+    return "result";
+  }
 }
-
-
-//public class PageController {
-//
-//  @GetMapping("/index")
-//  public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-//    model.addAttribute("name", name);
-//    return "greeting";
-//  }
-//
-//}
--- src/main/java/com/polling/controller/PollController.java
+++ src/main/java/com/polling/controller/PollController.java
@@ -1,53 +1,72 @@
-package com.polling;
+package com.polling.controller;
 
 import com.polling.model.Option;
+import com.polling.model.Sample;
 import com.polling.model.Poll;
 import com.polling.service.OptionService;
+import com.polling.service.PollService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.LinkedList;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/")
 public class PollController {
 
+  private final PollService pollService;
   private final OptionService optionService;
 
   @Autowired
-  public PollController(OptionService optionService) {
+  public PollController(PollService pollService, OptionService optionService) {
+    this.pollService = pollService;
     this.optionService = optionService;
   }
 
-  // create a new poll
-  @PostMapping(path = "create")
-  public void createPoll (@RequestBody Poll poll) throws Exception{
+  /**
+   * Create a new poll. If every thing is alright, a new poll along with all the questions and options will
+   * be inserted into the DB. Otherwise, an exception shall be raised.
+   */
+  @PostMapping(path = "/create")
+  public String createPoll (@RequestBody Poll poll) throws Exception{
     System.out.println("----------------create-------------------");
+    poll.show();
 
+    poll.setStartTime("2045-06-2 15:30:00");
+    poll.setEndTime("2045-06-6 15:30:00");
 
-//    ObjectMapper objectMapper = new ObjectMapper();
-//    objectMapper.readValue(poll, Poll.class);
+    try {
+      this.pollService.insertPoll(poll);
+      if (poll.getId() == null) throw new Exception("creation failed");
+      return poll.getId();
+    } catch (Exception e) {
+      throw new Exception("creation failed");
+    }
   }
 
-  // create a new note
-  @PostMapping(path = "t1")
-  public void t1 (@RequestBody @Valid Option op) throws Exception{
-    System.out.println("----------------create-------------------");
-    System.out.println(op);
+  /**
+   * Take a poll. If every thing is alright, changes shall be made in DB.
+   * Otherwise, an exception shall be raised.
+   */
+  @PostMapping(path = "/take_poll/{poll_id}")
+  public void takePoll (@RequestBody Sample sample, @PathVariable("poll_id") String poll_id) throws Exception{
 
+    Poll poll = this.pollService.findPollById(poll_id);
 
+    if (poll == null) throw new Exception("The poll does not exist");
 
+    LinkedList<String> optionIds = sample.getOptionIds();
+    LinkedList<Option> options = new LinkedList<>();
 
+    for (String id : optionIds) {
+      Option target = this.optionService.findOptionById(UUID.fromString(id));
+      if (target == null) throw new Exception("invalid ID found, submit failed");
+      options.add(target);
+    }
 
-
-
-
-
-
+    for (Option target : options) {
+      this.optionService.inc(target);
+    }
   }
-
-
 }
