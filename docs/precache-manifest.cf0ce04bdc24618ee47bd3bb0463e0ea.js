d816c86a9e64f5cce2351cdb4e5ae90199cc62a0
fuhuan1991 <huanfu2019@gmail.com>
2020-05-20 11:00:04 -0400

    build for gitHub page

---
+++ docs/precache-manifest.cf0ce04bdc24618ee47bd3bb0463e0ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2aa3c16207626ad97adf796f4ac7b38",
+    "url": "/EasyPoll/index.html"
+  },
+  {
+    "revision": "d3b083db60ca6baec6ef",
+    "url": "/EasyPoll/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "64cdaab88d0fba52c5d8",
+    "url": "/EasyPoll/static/css/main.c57749aa.chunk.css"
+  },
+  {
+    "revision": "d3b083db60ca6baec6ef",
+    "url": "/EasyPoll/static/js/2.c5f91634.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/EasyPoll/static/js/2.c5f91634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64cdaab88d0fba52c5d8",
+    "url": "/EasyPoll/static/js/main.70176add.chunk.js"
+  },
+  {
+    "revision": "ac187af4bcca780440ba",
+    "url": "/EasyPoll/static/js/runtime-main.24b20f1b.js"
+  },
+  {
+    "revision": "70bcac4dfde5f3823941c28249ea7403",
+    "url": "/EasyPoll/static/media/bg.70bcac4d.jpg"
+  }
+]);
